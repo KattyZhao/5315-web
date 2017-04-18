a740a85d23d5b16b41b7853f897a681d6c525101
Katty <sihuizhao@KattydeMacBook.local>
2017-04-17 20:13:27 -0700

    hw2 commit

--- empdata.js
+++ empdata.js
@@ -1,124 +1,77 @@
 {
   "data": [
     {
-      "name": "Garrett Winters",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "name": "Stephan Roch",
+      "birth_year": "1993",
+      "birth_month": "April",
+      "country": "US",
+      "GPA": "3.2",
+      "city": "Seattle"
     },
     {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
+      "name": "Katrina Phi",
+      "birth_year": "1990",
+      "birth_month": "March",
+      "country": "EU",
+      "GPA": "3.3",
+      "city": "Bellevue"
     },
     {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
+      "name": "Fox Christ",
+      "birth_year": "1989",
+      "birth_month": "Feb",
+      "country": "US",
+      "GPA": "3.1",
+      "city": "Seattle"
     },
     {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
+      "name": "Wenting Yuan",
+      "birth_year": "1993",
+      "birth_month": "June",
+      "country": "China",
+      "GPA": "3.8",
+      "city": "Seattle"
     },
     {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
+      "name": "Zichen Tia",
+      "birth_year": "1994",
+      "birth_month": "December",
+      "country": "UK",
+      "GPA": "3.2",
+      "city": "Bellevue"
     },
     {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
+      "name": "Zixuan Lin",
+      "birth_year": "1990",
+      "birth_month": "February",
+      "country": "China",
+      "GPA": "3.5",
+      "city": "Lynnwood"
     },
     {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
+      "name": "Yishan Zhang",
+      "birth_year": "1988",
+      "birth_month": "September",
+      "country": "US",
+      "GPA": "4.0",
+      "city": "Bellevue"
     },
     {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
+      "name": "Hong Wang",
+      "birth_year": "1994",
+      "birth_month": "April",
+      "country": "China",
+      "GPA": "4.0",
+      "city": "Seattle"
     },
     {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
+      "name": "Sihui Zhao",
+      "birth_year": "1993",
+      "birth_month": "April",
+      "country": "US",
+      "GPA": "3.8",
+      "city": "Mercer Island"
     },
-    {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
-    },
-    {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
-    },
-    {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
-    },
-    {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
-    },
-    {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
-    },
-    {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
-    }
+    
   ]
 }
